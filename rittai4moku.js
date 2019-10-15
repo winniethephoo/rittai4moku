27709d43ca785e4847c1372e3b7e89a06f444cbe
沢田悠佳 <YukaSawata@yuka-no-MacBook-Air.local>
2019-10-15 15:01:29 +0900

    first commit

--- rittai4moku.js
+++ rittai4moku.js
@@ -42,9 +42,95 @@ Vue.component('maru',{
     }
 })
 
+Vue.component('banmen',{
+    template: '<g> <entrance v-if="playing" v-for="(c,j) in board[0]" :j="j" @drop="shoot"></entrance>'+
+	' <g v-for="(row,i) in board">'+
+	'<maru v-for="(c,j) in row" :i="i" :j="j" :state="board[i][j]" ></maru>'+
+	'</g>'+
+	'</g>',
+    data: function(){
+	return {board:board,player:1,playing:true};
+	
+    },
+    methods:{
+	shoot(j){
+	    for(let i=board.length-1; i>=0; i--){
+		if(board[i][j]==0){
+		    board[i].splice(j,1,this.player);
+		    if(this.hantei(this.player)){
+			this.playing=false;
+			break;
+		    }
+		    
+		    if(this.player==1){
+			this.player=2;
+			
+		    }
+		    else  if(this.player==2){
+			
+			this.player=1;	
+		    }			
+		    break;
+		}
+	    }
+	}
+	,
+	hantei(player){
+	    for(let j=0; j<7; j++){
+		for(let i=0; i<3; i++){
+		    let flag=true;
+		    for(let k=0; k<4; k++){
+			console.log([i,j,k]);
+			if(board[i+k][j]!=this.player){
+			    flag=false;
+			    break;
+			}
+			
+		    }
+		    if (flag==true){
+			return true;
+		    }
+		}
+	    }
+	    for(let i=0; i<6; i++){
+		for(let j=0; j<4; j++){
+		    let flag=true;
+		    for(let k=0; k<4; k++){
+			console.log([i,j,k]);
+			if(board[i][j+k]!=this.player){
+			    flag=false;
+			    break;
+			}			
+		    }
+		    if (flag==true){
+			return true;
+		    }
+		}
+	    }
+	    for(let j=0; j<4; j++){
+		for(let i=0; i<3; i++){
+		    let flag=true;
+		    for(let k=1; k<4; k++){
+			console.log([i,j,k]);
+			if(board[i+k][j+k]!=this.player){
+			    flag=false;
+			    break;
+			}			
+		    }
+		    if (flag==true){
+			return true;
+		    }
+		}
+	    }
+	    return false;
+	}	
+    }    
+})
+
 app=new Vue({
     el:"#board",
     data:{
 	board: board,
+	player: 1,
     }
 });
